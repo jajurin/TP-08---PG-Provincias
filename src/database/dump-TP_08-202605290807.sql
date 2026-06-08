12f3c305ce24ded05de3e08a97b4276d59b9988d
Android Studio <devandroid@ort>
2026-06-08 11:20:22 -0300

    agregando readme

--- src/database/dump-TP_08-202605290807.sql
+++ src/database/dump-TP_08-202605290807.sql
@@ -83,7 +83,27 @@ COPY public.province (id, name, full_name, latitude, longitude, display_order) F
 20	Santa Fe	Provincia de Santa Fe	-31.6333	-60.7000	20
 \.
 
-
+--INSERT INTO public.province (name, full_name, latitude, longitude, display_order) VALUES
+--('Buenos Aires', 'Provincia de Buenos Aires', -36.6769, -60.5588, 1),
+--('Catamarca', 'Provincia de Catamarca', -28.4696, -65.7852, 2),
+--('Chaco', 'Provincia del Chaco', -26.3864, -60.7653, 3),
+--('Chubut', 'Provincia del Chubut', -43.3002, -65.1023, 4),
+--('Córdoba', 'Provincia de Córdoba', -31.4201, -64.1888, 5),
+--('Corrientes', 'Provincia de Corrientes', -27.4692, -58.8306, 6),
+--('Entre Ríos', 'Provincia de Entre Ríos', -31.7319, -60.5238, 7),
+--('Formosa', 'Provincia de Formosa', -26.1775, -58.1781, 8),
+--('Jujuy', 'Provincia de Jujuy', -24.1858, -65.2995, 9),
+--('La Pampa', 'Provincia de La Pampa', -36.6167, -64.2833, 10),
+--('La Rioja', 'Provincia de La Rioja', -29.4131, -66.8558, 11),
+--('Mendoza', 'Provincia de Mendoza', -32.8895, -68.8458, 12),
+--('Misiones', 'Provincia de Misiones', -27.3671, -55.8961, 13),
+--('Neuquén', 'Provincia del Neuquén', -38.9516, -68.0591, 14),
+--('Río Negro', 'Provincia de Río Negro', -40.8135, -63.0000, 15),
+--('Salta', 'Provincia de Salta', -24.7829, -65.4232, 16),
+--('San Juan', 'Provincia de San Juan', -31.5375, -68.5364, 17),
+--('San Luis', 'Provincia de San Luis', -33.2950, -66.3356, 18),
+--('Santa Cruz', 'Provincia de Santa Cruz', -51.6230, -69.2168, 19),
+--('Santa Fe', 'Provincia de Santa Fe', -31.6333, -60.7000, 20);
 --
 -- TOC entry 4786 (class 0 OID 0)
 -- Dependencies: 215
